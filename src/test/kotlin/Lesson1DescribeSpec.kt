a4d8b3c471db1d79f17704adee69e7f6f8c9b8a6
AltaAltynai <a.kanatpayeva@gmail.com>
2019-02-19 12:22:37 +0600

    QA: Engineer boundary tests MinOf vararg function other minor

--- src/test/kotlin/Lesson1DescribeSpec.kt
+++ src/test/kotlin/Lesson1DescribeSpec.kt
@@ -4,8 +4,10 @@ import io.kotlintest.matchers.numerics.shouldBeGreaterThan
 import io.kotlintest.matchers.numerics.shouldBeGreaterThanOrEqual
 import io.kotlintest.matchers.numerics.shouldBeLessThan
 import io.kotlintest.matchers.numerics.shouldBeLessThanOrEqual
+import io.kotlintest.matchers.types.shouldBeTypeOf
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.DescribeSpec
+
 
 class BasicsDescribeSpec : DescribeSpec({
     describe("Checks on Kotlin basics implementations") {
@@ -26,7 +28,6 @@ class BasicsDescribeSpec : DescribeSpec({
         }
 
         context("variables") {
-
             val readOnly = 11
             var reassignable = 3
 //            it("val can not be reassigned") {
@@ -66,7 +67,7 @@ class BasicsDescribeSpec : DescribeSpec({
             }
         }
 
-            // Write minOf function
+            // Write minOf function: 2 values
         context("minOf function") {
             val name1 = "Alice"
             val name2 = "Bob"
@@ -76,14 +77,35 @@ class BasicsDescribeSpec : DescribeSpec({
             }
         }
 
-            // Write *minOf function
-        context ("*minOf function") {
-            val list1 = listOf("x", "y", "z")
-            val list2 = listOf("a", "b", "c", "d")
-            val list3 = listOf("a", "b")
+            // Write *minOf function: 3 values
+
+        context("*minOf function: 3 values_1") {
+            val x:Int = -3
+            val y:Int = 5
+            val z:Int = 10
+            it("returns min") {
+
+                val min: Int = if(x <= y && x <= z) {
+                    x
+                }
+                else if(z <= x && z <= y) {
+                    z
+                }
+                else {
+                    y
+                }
+
+                min shouldBe -3
+            }
+        }
+
+        context("*minOf function: 3 values_2") {
+            val a = 1
+            val b = 4
+            val c = -1
 
             it("returns min") {
-                minOf(list1, list2, list3, compareBy { it.size }) shouldBe list3
+                minOf(a, b, c, compareBy { it }) shouldBe c
             }
         }
 
@@ -107,14 +129,54 @@ class BasicsDescribeSpec : DescribeSpec({
             }
         }
 
-            // Write mapOf function
-        context ("mapOf function") {
-            val fruits = listOf("Avocado", "Banana", "Apple", "Avocado")
+            // Write mapOf function for fruits
+        context("collections: mapOf function") {
+            val fruits = listOf("Banana", "Apple", "Avocado", "Watermelon", "Avocado")
 
             it("returns mapOf<String, Int>") {
                fruits.groupingBy{it}.eachCount()["Avocado"] shouldBe 2
             }
         }
 
+        // QA : Engineer check boundary values
+        context("boundary values") {
+
+            val features = arrayListOf("Homepage", "Registration flow", "Login", "Forgot password")
+
+            it ("checks releaseTesting Type")
+            QA(3, "Altynai", "QA").releaseTesting(features).shouldBeTypeOf<Boolean>()
+
+            it("checks daysLeft = features.size / 2")
+            QA(2, "Altynai", "QA").releaseTesting(features) shouldBe true
+
+            it("checks daysLeft > features.size / 2")
+            QA(10, "Altynai", "QA").releaseTesting(features) shouldBe true
+
+            it("checks daysLeft = 0")
+            QA(0, "Altynai", "QA").releaseTesting(features) shouldBe false
+
+            it("checks daysLeft < 0")
+            QA(-10, "Altynai", "QA").releaseTesting(features) shouldBe false
+
+            it("checks daysLeft < features.size / 2")
+            QA(1, "Altynai", "QA").releaseTesting(features) shouldBe false
+        }
+
     }
 })
+
+// MinOf vararg function
+fun minNumber(vararg numbers: Int) : Int? {
+    var smallest = 0
+
+    return if (numbers.isEmpty()) {
+        println("Error! Data input is required.")
+        return null
+    }  else {
+        smallest = numbers[0]
+        for (i in numbers) if (smallest > i) {
+            smallest = i
+        }
+        smallest
+    }
+}
